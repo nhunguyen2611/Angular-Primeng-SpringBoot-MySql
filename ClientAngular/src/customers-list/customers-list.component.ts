30db25af4656c9f0c8bbc54d89ed4ce11befbd16
Nikki <mieutieunha@gmail.com>
2018-10-31 13:53:41 +0700

    Add files via upload

---
+++ ClientAngular/src/customers-list/customers-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Customer } from '../customer';
+import { CustomerService } from '../customer.service';
+
+@Component({
+  selector: 'app-customers-list',
+  templateUrl: './customers-list.component.html',
+  styleUrls: ['./customers-list.component.scss']
+})
+export class CustomersListComponent implements OnInit {
+ 
+
+  customers: Observable<Customer[]>;
+
+  constructor(private customerService: CustomerService) { }
+
+  ngOnInit() {
+    this.reloadData();
+  }
+  deleteCustomers() {
+    this.customerService.deleteAll()
+      .subscribe(
+        data => {
+          console.log(data);
+          this.reloadData();
+        },
+        error => console.log('ERROR: ' + error));
+  }
+  reloadData() {
+    this.customers = this.customerService.getCustomersList();
+  }
+
+}
